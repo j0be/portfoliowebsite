f1347f46804f8f6aa2c51ffe19d6dc0d10ce8a1d
Ronald Troyer <rtroyer@aclens.local>
2017-12-07 17:35:22 -0500

    Fixing the icons.

--- views/social.php
+++ views/social.php
@@ -1,13 +1,15 @@
 <section name="social" class="social notch">
     <container>
-        <a class="socialitem github" href="http://github.com/j0be/" target="_blank" rel="noopener">Github</a>
-        <a class="socialitem codepen" href="https://codepen.io/j0be/" target="_blank" rel="noopener">Codepen</a>
-        <a class="socialitem linkedin" href="https://www.linkedin.com/in/ron-troyer-b3642622/" target="_blank" rel="noopener">LinkedIn</a>
-        <span class="socialitem inactive dribbble" target="_blank" rel="noopener">
-            Dribble
-            <span>If you have an extra invite laying around, you'd be my favorite if you could invite me</span>
+        <span class="socialitem"><a class="github" href="http://github.com/j0be/" target="_blank" rel="noopener">Github</a></span>
+        <span class="socialitem"><a class="codepen" href="https://codepen.io/j0be/" target="_blank" rel="noopener">Codepen</a></span>
+        <span class="socialitem"><a class="linkedin" href="https://www.linkedin.com/in/ron-troyer-b3642622/" target="_blank" rel="noopener">LinkedIn</a></span>
+        <span class="socialitem">
+            <span class="inactive dribbble" target="_blank" rel="noopener">
+                Dribble
+                <span class="hover">If you have an extra invite laying around, you'd be my favorite if you could invite me</span>
+            </span>
         </span>
-        <a class="socialitem reddit" href="https://www.reddit.com/user/j0be" target="_blank" rel="noopener">Reddit</a>
-        <a class="socialitem imgur" href="https://imgur.com/user/j0be" target="_blank" rel="noopener">Imgur</a>
+        <span class="socialitem"><a class="reddit" href="https://www.reddit.com/user/j0be" target="_blank" rel="noopener">Reddit</a></span>
+        <span class="socialitem"><a class="imgur" href="https://imgur.com/user/j0be" target="_blank" rel="noopener">Imgur</a></span>
     </container>
 </section>
