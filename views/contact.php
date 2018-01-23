13661c0fdcafe094e2bc2c92f6949cd53d7d8165
Ronald Troyer <ron.troyer@gmail.com>
2018-01-23 07:41:18 -0500

    Removing two social links until I get the js side working

--- views/contact.php
+++ views/contact.php
@@ -9,8 +9,6 @@
         <a href="https://github.com/j0be" target="_blank" rel="nofollow">Github</a>
         <a href="https://codepen.io/j0be" target="_blank" rel="nofollow">Codepen</a>
         <a href="https://www.linkedin.com/in/ron-troyer/" target="_blank" rel="nofollow">LinkedIn</a>
-        <a href="https://reddit.com/user/j0be" target="_blank" rel="nofollow">Reddit</a>
-        <a href="https://imgur.com/user/j0be/submitted/best" target="_blank" rel="nofollow">Imgur</a>
         <a href="mailto:ron.troyer@gmail.com" target="_blank" rel="nofollow">Email</a>
     </div>
 </section>
