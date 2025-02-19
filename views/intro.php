664bc3fe17d8c8a8173fcde9ab401136a40c1111
Ronald Troyer <ron.troyer@gmail.com>
2025-02-19 16:30:05 -0500

    Update the current status of things

--- views/intro.php
+++ views/intro.php
@@ -10,22 +10,20 @@
         </nav>
     </header>
     <div class="summary">
-        I'm Ronald Troyer, a <a href="http://github.com/j0be" target="_blank" rel="nofollow">web developer</a>
-        and <a href="http://codepen.io/j0be" target="_blank" rel="nofollow">web designer</a> living in Columbus, Ohio
-        currently working for AC Lens.
+        I'm Ronald Troyer, a <a href="https://github.com/j0be" target="_blank" rel="nofollow">web developer</a> and <a href="https://codepen.io/j0be" target="_blank" rel="nofollow">web designer</a> living in Columbus, Ohio looking for work.
     </div>
     <div class="experience">
         <div class="item">
-            <span>Currently:</span>
-            <a href="http://aclens.com" target="_blank" rel="nofollow" class="aclens" data-target="#aclens">AC Lens</a>
+            <span>Recently:</span>
+            <a href="https://chase.com" target="_blank" rel="nofollow" class="aclens" data-target="#aclens">AC Lens</a>
         </div>
         <div class="item">
             <span>Previously:</span>
-            <a href="http://occrra.org" target="_blank" rel="nofollow" class="occrra" data-target="#occrra">OCCRRA</a>
+            <a href="https://aclens.com" target="_blank" rel="nofollow" class="aclens" data-target="#aclens">AC Lens</a>
         </div>
         <div class="item">
             <span>Before that:</span>
-            <a href="http://express.com" target="_blank" rel="nofollow" class="express" data-target="#express">Express</a>
+            <a href="https://occrra.org" target="_blank" rel="nofollow" class="occrra" data-target="#occrra">OCCRRA</a>
         </div>
     </div>
 </section>
